c11b65d8700de44379b3a7ccc61429b3687129c4
Mpho <matebesimoe89@gmail.com>
2026-07-16 21:35:08 +0200

    added a create dim customer table script

---
+++ 1.0.scripts_sql/1.3.dim_tables/1.3.2.dim_customer.sql
@@ -0,0 +1,15 @@
+-- Create table if it does not exist
+IF OBJECT_ID('[stg_bright_mart_sales].[dbo].[dim_customer]', 'U') IS NULL
+
+    CREATE TABLE [stg_bright_mart_sales].[dbo].[dim_customer](
+        [CustomerID] INT IDENTITY(1, 1) PRIMARY KEY,
+        [customer_first_name] [varchar](50) NULL,
+		[customer_last_name] [varchar](50) NULL,
+		[customer_email] [varchar](50) NULL,
+		[customer_phone] [varchar](50) NULL,
+		[customer_city] [varchar](50) NULL,
+		[customer_province] [varchar](50) NULL,
+		[customer_loyalty_tier] [varchar](50) NULL,
+        [load_date] DATETIME DEFAULT GETDATE()
+    );
+GO
