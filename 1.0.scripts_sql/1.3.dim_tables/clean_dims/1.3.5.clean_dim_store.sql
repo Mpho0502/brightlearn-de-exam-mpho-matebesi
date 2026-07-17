dd0197f8703cca3f3bb3e0d9e8c20e9b96a1b95b
Mpho <matebesimoe89@gmail.com>
2026-07-17 20:19:36 +0200

    added a create clean dim store table script

---
+++ 1.0.scripts_sql/1.3.dim_tables/clean_dims/1.3.5.clean_dim_store.sql
@@ -0,0 +1,14 @@
+--Recreate a table with proper DATE data types set to NOT NULL
+IF OBJECT_ID('[stg_bright_mart_sales].[dbo].[clean_dim_store]', 'U') IS NULL
+    CREATE TABLE [stg_bright_mart_sales].[dbo].[clean_dim_store](
+        [StoreID]        INT IDENTITY(1, 1) PRIMARY KEY,
+        [store_name]     VARCHAR(50) NOT NULL,
+        [store_city]     VARCHAR(50) NOT NULL,
+        [store_province] VARCHAR(50) NOT NULL,
+        [store_region]   VARCHAR(50) NOT NULL,
+        [store_manager]  VARCHAR(50) NOT NULL,
+        [cashier_name]   VARCHAR(50) NOT NULL,
+        [load_date]      DATETIME DEFAULT GETDATE()
+    );
+
+
