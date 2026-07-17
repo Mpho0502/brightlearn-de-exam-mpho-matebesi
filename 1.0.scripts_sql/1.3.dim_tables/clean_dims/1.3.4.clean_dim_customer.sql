9b17de311691db761a61e655a0d702cce34fc4c6
Mpho <matebesimoe89@gmail.com>
2026-07-17 20:07:41 +0200

    inserted distinct values using where not exist into clean dim customer from raw data

--- 1.0.scripts_sql/1.3.dim_tables/clean_dims/1.3.4.clean_dim_customer.sql
+++ 1.0.scripts_sql/1.3.dim_tables/clean_dims/1.3.4.clean_dim_customer.sql
@@ -12,3 +12,42 @@ IF OBJECT_ID('[stg_bright_mart_sales].[dbo].[clean_dim_customer]', 'U') IS NULL
         [load_date]             DATETIME DEFAULT GETDATE()
     );
 
+    -- Insert distinct values using WHERE NOT EXISTS and handling blanks/NULLs
+INSERT INTO [stg_bright_mart_sales].[dbo].[clean_dim_customer] ( 
+    [customer_first_name], 
+    [customer_last_name], 
+    [customer_email], 
+    [customer_phone], 
+    [customer_city], 
+    [customer_province], 
+    [customer_loyalty_tier] 
+) 
+SELECT DISTINCT 
+    -- Clean whitespaces, catch empty blocks, and swap NULLs with 'Unknown'
+    COALESCE(NULLIF(TRIM([customer_first_name]), ''), 'Unknown')   AS [customer_first_name], 
+    COALESCE(NULLIF(TRIM([customer_last_name]), ''), 'Unknown')    AS [customer_last_name], 
+    COALESCE(NULLIF(TRIM([customer_email]), ''), 'Unknown')        AS [customer_email], 
+    COALESCE(NULLIF(TRIM([customer_phone]), ''), 'Unknown')        AS [customer_phone], 
+    COALESCE(NULLIF(TRIM([customer_city]), ''), 'Unknown')         AS [customer_city], 
+    COALESCE(NULLIF(TRIM([customer_province]), ''), 'Unknown')     AS [customer_province], 
+    COALESCE(NULLIF(TRIM([customer_loyalty_tier]), ''), 'Unknown') AS [customer_loyalty_tier] 
+FROM [stg_bright_mart_sales].[dbo].[bright_mart_raw_data] AS srd
+WHERE ([customer_first_name] IS NOT NULL OR 
+       [customer_last_name] IS NOT NULL OR 
+       [customer_email] IS NOT NULL OR 
+       [customer_phone] IS NOT NULL OR 
+       [customer_city] IS NOT NULL OR 
+       [customer_province] IS NOT NULL OR 
+       [customer_loyalty_tier] IS NOT NULL)
+  AND NOT EXISTS (
+      SELECT 1 
+      FROM [stg_bright_mart_sales].[dbo].[clean_dim_customer] AS cdc
+      WHERE cdc.[customer_first_name]   = COALESCE(NULLIF(TRIM(srd.[customer_first_name]), ''), 'Unknown')
+        AND cdc.[customer_last_name]    = COALESCE(NULLIF(TRIM(srd.[customer_last_name]), ''), 'Unknown')
+        AND cdc.[customer_email]        = COALESCE(NULLIF(TRIM(srd.[customer_email]), ''), 'Unknown')
+        AND cdc.[customer_phone]        = COALESCE(NULLIF(TRIM(srd.[customer_phone]), ''), 'Unknown')
+        AND cdc.[customer_city]         = COALESCE(NULLIF(TRIM(srd.[customer_city]), ''), 'Unknown')
+        AND cdc.[customer_province]     = COALESCE(NULLIF(TRIM(srd.[customer_province]), ''), 'Unknown')
+        AND cdc.[customer_loyalty_tier] = COALESCE(NULLIF(TRIM(srd.[customer_loyalty_tier]), ''), 'Unknown')
+  );
+
