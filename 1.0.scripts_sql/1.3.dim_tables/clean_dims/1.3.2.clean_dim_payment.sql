c004b036d373000cb9bc294c6221b1e94cf3e815
Mpho <matebesimoe89@gmail.com>
2026-07-17 18:36:55 +0200

    inserted distinct values using where not exist into clean dim payment from raw data

--- 1.0.scripts_sql/1.3.dim_tables/clean_dims/1.3.2.clean_dim_payment.sql
+++ 1.0.scripts_sql/1.3.dim_tables/clean_dims/1.3.2.clean_dim_payment.sql
@@ -9,3 +9,19 @@ BEGIN
 END;
 GO
 
+-- Insert distinct values using WHERE NOT EXISTS and handling NULLs
+INSERT INTO [stg_bright_mart_sales].[dbo].[clean_dim_payment] ( 
+    [payment_method] 
+) 
+SELECT DISTINCT 
+    -- If the payment method is missing or NULL, fallback to an 'Unknown' placeholder
+    COALESCE([payment_method], 'Unknown') AS [payment_method] 
+FROM [stg_bright_mart_sales].[dbo].[bright_mart_raw_data] AS srd
+WHERE [payment_method] IS NOT NULL
+  AND NOT EXISTS (
+      SELECT 1 
+      FROM [stg_bright_mart_sales].[dbo].[clean_dim_payment] AS cdp
+      WHERE cdp.[payment_method] = COALESCE(srd.[payment_method], 'Unknown')
+  );
+GO
+
