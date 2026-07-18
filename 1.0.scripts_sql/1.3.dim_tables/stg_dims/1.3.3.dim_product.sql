df2e820c59fc42016dcf11e69ef51c25ca18f97a
Mpho <matebesimoe89@gmail.com>
2026-07-18 12:49:30 +0200

    fixed code to insert distinct values only if they do not already exist to dim_product

--- 1.0.scripts_sql/1.3.dim_tables/stg_dims/1.3.3.dim_product.sql
+++ 1.0.scripts_sql/1.3.dim_tables/stg_dims/1.3.3.dim_product.sql
@@ -12,21 +12,30 @@ IF OBJECT_ID('[stg_bright_mart_sales].[dbo].[dim_product]', 'U') IS NULL
     );
 GO
 
--- Insert distinct values into the table from raw data
+-- Insert distinct values into the table from raw data only if they do not already exist
 INSERT INTO [stg_bright_mart_sales].[dbo].[dim_product] (
-		[supplier],
-		[product_name],
-	    [category],
-	    [sub_category],
-	    [sku]
+        [supplier],
+        [product_name],
+        [category],
+        [sub_category],
+        [sku]
 )
 SELECT DISTINCT
-        [supplier],
-		[product_name],
-	    [category],
-	    [sub_category],
-	    [sku]
-FROM [stg_bright_mart_sales].[dbo].[bright_mart_raw_data];
+        raw.[supplier],
+        raw.[product_name],
+        raw.[category],
+        raw.[sub_category],
+        raw.[sku]
+FROM [stg_bright_mart_sales].[dbo].[bright_mart_raw_data] raw
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM [stg_bright_mart_sales].[dbo].[dim_product] dim
+    WHERE ISNULL(dim.[supplier], '')     = ISNULL(raw.[supplier], '')
+      AND ISNULL(dim.[product_name], '') = ISNULL(raw.[product_name], '')
+      AND ISNULL(dim.[category], '')     = ISNULL(raw.[category], '')
+      AND ISNULL(dim.[sub_category], '') = ISNULL(raw.[sub_category], '')
+      AND ISNULL(dim.[sku], '')          = ISNULL(raw.[sku], '')
+);
 
 -- Show the dim product table
 SELECT *
