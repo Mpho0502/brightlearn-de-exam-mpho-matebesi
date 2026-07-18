cb745c216f592e05a4462eb33c320727a1b8f0ce
Mpho <matebesimoe89@gmail.com>
2026-07-18 12:53:38 +0200

    fixed code to insert distinct values only if they do not already exist to dim_date

--- 1.0.scripts_sql/1.3.dim_tables/stg_dims/1.3.5.dim_date.sql
+++ 1.0.scripts_sql/1.3.dim_tables/stg_dims/1.3.5.dim_date.sql
@@ -9,16 +9,23 @@ IF OBJECT_ID('[stg_bright_mart_sales].[dbo].[dim_date]', 'U') IS NULL
     );
 GO
 
--- Insert distinct values into the table from raw data
+-- Insert distinct values into the table from raw data only if they do not already exist
 INSERT INTO [stg_bright_mart_sales].[dbo].[dim_date] (
         [transaction_date],
         [customer_since]
 )
 SELECT DISTINCT
-        [transaction_date],
-        [customer_since]
-FROM [stg_bright_mart_sales].[dbo].[bright_mart_raw_data];
+        raw.[transaction_date],
+        raw.[customer_since]
+FROM [stg_bright_mart_sales].[dbo].[bright_mart_raw_data] raw
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM [stg_bright_mart_sales].[dbo].[dim_date] dim
+    WHERE ISNULL(dim.[transaction_date], '') = ISNULL(raw.[transaction_date], '')
+      AND ISNULL(dim.[customer_since], '')    = ISNULL(raw.[customer_since], '')
+);
 
 -- Show the dim date table
 SELECT *
 FROM [stg_bright_mart_sales].[dbo].[dim_date];
+
