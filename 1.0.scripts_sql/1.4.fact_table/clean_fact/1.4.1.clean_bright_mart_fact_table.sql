0c27cf8a94440e5647c4c5a2d0ee3f8ce06346a5
Mpho <matebesimoe89@gmail.com>
2026-07-18 12:59:22 +0200

    added a create clean bright mart fact table script

---
+++ 1.0.scripts_sql/1.4.fact_table/clean_fact/1.4.1.clean_bright_mart_fact_table.sql
@@ -0,0 +1,14 @@
+--Recreate a table with proper DATE data types instead of VARCHAR
+IF OBJECT_ID('[stg_bright_mart_sales].[dbo].[clean_bright_mart_fact_table]', 'U') IS NULL
+CREATE TABLE [stg_bright_mart_sales].[dbo].[clean_bright_mart_fact_table](
+        [SalesID]              INT IDENTITY(1, 1) PRIMARY KEY,
+        [unit_price]           DECIMAL(18, 2) NOT NULL, -- Converted to standard money/decimal format
+        [cost_price]           DECIMAL(18, 2) NOT NULL,
+        [qty]                  INT NOT NULL,            -- Converted to integer for whole items
+        [line_amount]          DECIMAL(18, 2) NOT NULL,
+        [stock_on_hand]        INT NOT NULL,
+        [reorder_threshold]    INT NOT NULL,
+        [transaction_amount]   DECIMAL(18, 2) NOT NULL,
+        [transaction_discount] DECIMAL(18, 2) NOT NULL,
+        [load_date]            DATETIME DEFAULT GETDATE()
+    );
