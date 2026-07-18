e1bc0e5f6278705ac2764b9a4a8984dc6d19572b
Mpho <matebesimoe89@gmail.com>
2026-07-18 12:51:08 +0200

    fixed code to insert distinct values only if they do not already exist to dim_payment

--- 1.0.scripts_sql/1.3.dim_tables/stg_dims/1.3.4.dim_payment.sql
+++ 1.0.scripts_sql/1.3.dim_tables/stg_dims/1.3.4.dim_payment.sql
@@ -8,13 +8,18 @@ IF OBJECT_ID('[stg_bright_mart_sales].[dbo].[dim_payment]', 'U') IS NULL
     );
 GO
 
--- Insert distinct values into the table from raw data
+-- Insert distinct values into the table from raw data only if they do not already exist
 INSERT INTO [stg_bright_mart_sales].[dbo].[dim_payment] (
-		[payment_method]
+        [payment_method]
 )
 SELECT DISTINCT
-        [payment_method]
-FROM [stg_bright_mart_sales].[dbo].[bright_mart_raw_data];
+        raw.[payment_method]
+FROM [stg_bright_mart_sales].[dbo].[bright_mart_raw_data] raw
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM [stg_bright_mart_sales].[dbo].[dim_payment] dim
+    WHERE ISNULL(dim.[payment_method], '') = ISNULL(raw.[payment_method], '')
+);
 
 -- Show the dim product table
 SELECT *
