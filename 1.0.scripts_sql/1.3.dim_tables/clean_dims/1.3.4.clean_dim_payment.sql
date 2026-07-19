c76f47928f652c7c5068abe8f90074e8d9a15872
Mpho <matebesimoe89@gmail.com>
2026-07-19 11:57:34 +0200

    aligned the clean dim payment script

--- 1.0.scripts_sql/1.3.dim_tables/clean_dims/1.3.4.clean_dim_payment.sql
+++ 1.0.scripts_sql/1.3.dim_tables/clean_dims/1.3.4.clean_dim_payment.sql
@@ -1,29 +1,25 @@
 -- Recreate a table with proper DATE data types set to NOT NULL
 IF OBJECT_ID('[stg_bright_mart_sales].[dbo].[clean_dim_payment]', 'U') IS NULL
-BEGIN
     CREATE TABLE [stg_bright_mart_sales].[dbo].[clean_dim_payment](
         [PaymentID]      INT IDENTITY(1, 1) PRIMARY KEY,
         [payment_method] VARCHAR(50) NOT NULL, --  set to provide a fallback
         [load_date]      DATETIME DEFAULT GETDATE()
-    );
-END;
-GO
+ );
 
 -- Insert distinct values using WHERE NOT EXISTS and handling NULLs
 INSERT INTO [stg_bright_mart_sales].[dbo].[clean_dim_payment] ( 
-    [payment_method] 
+        [payment_method] 
 ) 
 SELECT DISTINCT 
     -- If the payment method is missing or NULL, fallback to an 'Unknown' placeholder
     COALESCE([payment_method], 'Unknown') AS [payment_method] 
 FROM [stg_bright_mart_sales].[dbo].[bright_mart_raw_data] AS srd
-WHERE [payment_method] IS NOT NULL
+WHERE  [payment_method] IS NOT NULL
   AND NOT EXISTS (
       SELECT 1 
       FROM [stg_bright_mart_sales].[dbo].[clean_dim_payment] AS cdp
       WHERE cdp.[payment_method] = COALESCE(srd.[payment_method], 'Unknown')
-  );
-GO
+);
 
 -- Show the clean table to verify the data types and contents 
 SELECT * FROM [stg_bright_mart_sales].[dbo].[clean_dim_payment];
