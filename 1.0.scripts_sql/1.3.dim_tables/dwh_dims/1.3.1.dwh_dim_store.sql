a5f4a7c8e9cbe2392279f0d7425292817b839e45
Mpho <matebesimoe89@gmail.com>
2026-07-19 11:18:43 +0200

    inserted distinct values using where not exist to dwh dim store

--- 1.0.scripts_sql/1.3.dim_tables/dwh_dims/1.3.1.dwh_dim_store.sql
+++ 1.0.scripts_sql/1.3.dim_tables/dwh_dims/1.3.1.dwh_dim_store.sql
@@ -11,3 +11,30 @@ IF OBJECT_ID('[dwh_bright_mart_sales].[dbo].[dwh_dim_store]', 'U') IS NULL
         [load_date]      DATETIME DEFAULT GETDATE()
     );
 
+-- Insert distinct values only if they do not already exist
+INSERT INTO [dwh_bright_mart_sales].[dbo].[dwh_dim_store] (
+        [store_name],
+        [store_city],
+        [store_province],
+        [store_region],
+        [store_manager],
+        [cashier_name]
+)
+SELECT DISTINCT
+        cln.[store_name],
+        cln.[store_city],
+        cln.[store_province],
+        cln.[store_region],
+        cln.[store_manager],
+        cln.[cashier_name]
+FROM [stg_bright_mart_sales].[dbo].[clean_dim_store] cln
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM [dwh_bright_mart_sales].[dbo].[dwh_dim_store] dim
+    WHERE ISNULL(dim.[store_name], '')     = ISNULL(cln.[store_name], '')
+      AND ISNULL(dim.[store_city], '')     = ISNULL(cln.[store_city], '')
+      AND ISNULL(dim.[store_province], '') = ISNULL(cln.[store_province], '')
+      AND ISNULL(dim.[store_region], '')   = ISNULL(cln.[store_region], '')
+      AND ISNULL(dim.[store_manager], '')  = ISNULL(cln.[store_manager], '')
+      AND ISNULL(dim.[cashier_name], '')   = ISNULL(cln.[cashier_name], '')
+);
