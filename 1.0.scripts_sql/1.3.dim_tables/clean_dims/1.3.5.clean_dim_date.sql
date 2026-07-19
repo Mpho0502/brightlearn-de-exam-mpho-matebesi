c6aadf8ca2d13d5ba7fc2e1310ec6e781b33ac8a
Mpho <matebesimoe89@gmail.com>
2026-07-19 12:38:12 +0200

    aligned the clean dim date script set datatype to not null

--- 1.0.scripts_sql/1.3.dim_tables/clean_dims/1.3.5.clean_dim_date.sql
+++ 1.0.scripts_sql/1.3.dim_tables/clean_dims/1.3.5.clean_dim_date.sql
@@ -2,22 +2,23 @@
 IF OBJECT_ID('[stg_bright_mart_sales].[dbo].[clean_dim_date]', 'U') IS NULL
     CREATE TABLE [stg_bright_mart_sales].[dbo].[clean_dim_date](
         [DateID] INT IDENTITY(1, 1) PRIMARY KEY,
-        [transaction_date] DATE NULL,      -- Converted to DATE type
-        [customer_since] DATE NULL,        -- Converted to DATE type
+        [transaction_date] DATE NOT NULL,      -- Converted to DATE type
+        [customer_since] DATE NOT NULL,        -- Converted to DATE type
         [load_date] DATETIME DEFAULT GETDATE()
 );
 
 -- Insert rows using WHERE NOT EXISTS to prevent duplicates across multiple execution runs
 INSERT INTO [stg_bright_mart_sales].[dbo].[clean_dim_date] ( 
-    [transaction_date], 
-    [customer_since] 
+        [transaction_date], 
+        [customer_since] 
 ) 
 SELECT DISTINCT 
     -- If conversion fails or raw data is NULL, fallback to a default '1900-01-01' date 
     COALESCE(TRY_CONVERT(DATE, [transaction_date]), '1900-01-01') AS [transaction_date], 
     COALESCE(TRY_CONVERT(DATE, [customer_since]), '1900-01-01') AS [customer_since] 
 FROM [stg_bright_mart_sales].[dbo].[bright_mart_raw_data] AS srd
-WHERE ([transaction_date] IS NOT NULL OR [customer_since] IS NOT NULL)
+WHERE ([transaction_date] IS NOT NULL OR 
+       [customer_since] IS NOT NULL)
   AND NOT EXISTS (
       SELECT 1 
       FROM [stg_bright_mart_sales].[dbo].[clean_dim_date] AS cdd
