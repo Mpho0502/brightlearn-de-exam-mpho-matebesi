84c44eb7bc6ff9b0316b1be567ed39dfc9ce5c95
Mpho <matebesimoe89@gmail.com>
2026-07-19 12:39:54 +0200

    aligned the dwh dim date script set datatype to not null

--- 1.0.scripts_sql/1.3.dim_tables/dwh_dims/1.3.5.dwh_dim_date.sql
+++ 1.0.scripts_sql/1.3.dim_tables/dwh_dims/1.3.5.dwh_dim_date.sql
@@ -2,8 +2,8 @@
 IF OBJECT_ID('[dwh_bright_mart_sales].[dbo].[dwh_dim_date]', 'U') IS NULL
     CREATE TABLE [dwh_bright_mart_sales].[dbo].[dwh_dim_date](
         [DateID] INT IDENTITY(1, 1) PRIMARY KEY,
-        [transaction_date] DATE NULL,      
-        [customer_since] DATE NULL,       
+        [transaction_date] DATE NOT NULL,      
+        [customer_since] DATE NOT NULL,       
         [load_date] DATETIME DEFAULT GETDATE()
 );
 
