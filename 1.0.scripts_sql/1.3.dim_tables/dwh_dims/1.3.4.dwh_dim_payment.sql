eb3d198653c2676eb8cc244ad9dc2e4d8933921a
Mpho <matebesimoe89@gmail.com>
2026-07-19 12:09:54 +0200

    inserted distinct values using where not exist to dwh dim payment

--- 1.0.scripts_sql/1.3.dim_tables/dwh_dims/1.3.4.dwh_dim_payment.sql
+++ 1.0.scripts_sql/1.3.dim_tables/dwh_dims/1.3.4.dwh_dim_payment.sql
@@ -6,4 +6,15 @@ IF OBJECT_ID('[dwh_bright_mart_sales].[dbo].[dwh_dim_payment]', 'U') IS NULL
         [load_date]      DATETIME DEFAULT GETDATE()
  );
 
-
+-- Insert distinct values into the table from raw data only if they do not already exist
+INSERT INTO [dwh_bright_mart_sales].[dbo].[dwh_dim_payment](
+        [payment_method]
+)
+SELECT DISTINCT
+        cln.[payment_method]
+FROM [stg_bright_mart_sales].[dbo].[clean_dim_payment] cln
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM [dwh_bright_mart_sales].[dbo].[dwh_dim_payment] dim
+    WHERE ISNULL(dim.[payment_method], '') = ISNULL(cln.[payment_method], '')
+);
