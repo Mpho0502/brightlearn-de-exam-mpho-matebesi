2e73e9e8aff7d6c562901f418d7de7f41e7d6d56
Mpho <matebesimoe89@gmail.com>
2026-07-19 20:24:46 +0200

    answer for business query BQ-01

--- 2.0.business_queries/BQ-01.sql
+++ 2.0.business_queries/BQ-01.sql
@@ -7,3 +7,14 @@
 
 --BQ-01:	What were the top 5 best-selling products by total revenue between January and June 2026?
 
+SELECT TOP 5
+    p.[product_name],
+    SUM(f.[transaction_amount]) AS total_revenue
+FROM [dwh_bright_mart_sales].[dbo].[dwh_bright_mart_fact_table] f
+JOIN [dwh_bright_mart_sales].[dbo].[dwh_dim_product] p
+    ON f.[ProductID] = p.[ProductID]
+JOIN [dwh_bright_mart_sales].[dbo].[dwh_dim_date] d
+    ON f.[DateID] = d.[DateID]
+WHERE d.[transaction_date] BETWEEN '2024-01-01' AND '2024-06-30'
+GROUP BY p.[product_name]
+ORDER BY total_revenue DESC;
