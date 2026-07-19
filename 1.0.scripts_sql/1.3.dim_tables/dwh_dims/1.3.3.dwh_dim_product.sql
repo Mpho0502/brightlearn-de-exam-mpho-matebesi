7d8a8d780c97806110b609950f210362ad616ae2
Mpho <matebesimoe89@gmail.com>
2026-07-19 11:41:29 +0200

    inserted distinct values using where not exist to dwh dim product

--- 1.0.scripts_sql/1.3.dim_tables/dwh_dims/1.3.3.dwh_dim_product.sql
+++ 1.0.scripts_sql/1.3.dim_tables/dwh_dims/1.3.3.dwh_dim_product.sql
@@ -9,3 +9,28 @@ IF OBJECT_ID('[dwh_bright_mart_sales].[dbo].[dwh_dim_product]', 'U') IS NULL
         [sku]          VARCHAR(50) NOT NULL,
         [load_date]    DATETIME DEFAULT GETDATE()
     );
+
+-- Insert distinct values into the table from raw data only if they do not already exist
+INSERT INTO [dwh_bright_mart_sales].[dbo].[dwh_dim_product] (
+        [supplier],
+        [product_name],
+        [category],
+        [sub_category],
+        [sku]
+)
+SELECT DISTINCT
+        cln.[supplier],
+        cln.[product_name],
+        cln.[category],
+        cln.[sub_category],
+        cln.[sku]
+FROM [stg_bright_mart_sales].[dbo].[clean_dim_product] cln
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM [dwh_bright_mart_sales].[dbo].[dwh_dim_product] dim
+    WHERE ISNULL(dim.[supplier], '')     = ISNULL(cln.[supplier], '')
+      AND ISNULL(dim.[product_name], '') = ISNULL(cln.[product_name], '')
+      AND ISNULL(dim.[category], '')     = ISNULL(cln.[category], '')
+      AND ISNULL(dim.[sub_category], '') = ISNULL(cln.[sub_category], '')
+      AND ISNULL(dim.[sku], '')          = ISNULL(cln.[sku], '')
+);
