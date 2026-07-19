5af24417bd708e41003c7c49e0080d0d9035b4bc
Mpho <matebesimoe89@gmail.com>
2026-07-19 17:41:39 +0200

    added a sp create dim store table script

---
+++ 1.0.scripts_sql/1.5.stored_procedures/1. sp_stg_tables/1.5.1.sp_create_dim_store.sql
@@ -0,0 +1,54 @@
+--Create a stored procedure for dim store in stg.
+CREATE OR ALTER PROCEDURE [dbo].[sp_create_dim_store]
+AS
+BEGIN
+    SET NOCOUNT ON; --speeds up ETL process,stops server from countinng rows affected
+
+    -- Create table if it does not exist
+    IF OBJECT_ID('[stg_bright_mart_sales].[dbo].[dim_store]', 'U') IS NULL
+    BEGIN
+        CREATE TABLE [stg_bright_mart_sales].[dbo].[dim_store](
+            [StoreID] INT IDENTITY(1, 1) PRIMARY KEY,
+            [store_name]     VARCHAR(50) NULL,
+            [store_city]     VARCHAR(50) NULL,
+            [store_province] VARCHAR(50) NULL,
+            [store_region]   VARCHAR(50) NULL,
+            [store_manager]  VARCHAR(50) NULL,
+            [cashier_name]   VARCHAR(50) NULL,
+            [load_date] DATETIME DEFAULT GETDATE()
+        );
+    END;
+
+    -- Insert distinct values only if they do not already exist
+    INSERT INTO [stg_bright_mart_sales].[dbo].[dim_store] (
+            [store_name],
+            [store_city],
+            [store_province],
+            [store_region],
+            [store_manager],
+            [cashier_name]
+    )
+    SELECT DISTINCT
+            raw.[store_name],
+            raw.[store_city],
+            raw.[store_province],
+            raw.[store_region],
+            raw.[store_manager],
+            raw.[cashier_name]
+    FROM [stg_bright_mart_sales].[dbo].[bright_mart_raw_data] raw
+    WHERE NOT EXISTS (
+        SELECT 1 
+        FROM [stg_bright_mart_sales].[dbo].[dim_store] dim
+        WHERE ISNULL(dim.[store_name], '')     = ISNULL(raw.[store_name], '')
+          AND ISNULL(dim.[store_city], '')     = ISNULL(raw.[store_city], '')
+          AND ISNULL(dim.[store_province], '') = ISNULL(raw.[store_province], '')
+          AND ISNULL(dim.[store_region], '')   = ISNULL(raw.[store_region], '')
+          AND ISNULL(dim.[store_manager], '')  = ISNULL(raw.[store_manager], '')
+          AND ISNULL(dim.[cashier_name], '')   = ISNULL(raw.[cashier_name], '')
+    );
+
+    -- Show the dim store table
+    SELECT *
+    FROM [stg_bright_mart_sales].[dbo].[dim_store];
+END;
+GO
