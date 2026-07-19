8017f1573a3492f8ab1aa4351c73c5f9099e8d02
Mpho <matebesimoe89@gmail.com>
2026-07-19 19:40:46 +0200

    added business queries folder and BQ-01 template script

---
+++ 2.0.business_queries/BQ-01.sql
@@ -0,0 +1,9 @@
+--Name: brightlearn-de-exam-mpho-matebesi  
+--Course: Data Engineering 
+--Project Name: BrightLearn Data Warehouse Build 
+--Project Code: BL-DE-EXAM-2026-07
+
+--Business Analytical Queries
+
+--BQ-01:	What were the top 5 best-selling products by total revenue between January and June 2026?
+
