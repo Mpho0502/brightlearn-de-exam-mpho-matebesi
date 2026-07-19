ec0b81721daa12aa1f029fafc424436ad76a825e
Mpho <matebesimoe89@gmail.com>
2026-07-19 12:31:54 +0200

    aligned the dim date script

--- 1.0.scripts_sql/1.3.dim_tables/stg_dims/1.3.5.dim_date.sql
+++ 1.0.scripts_sql/1.3.dim_tables/stg_dims/1.3.5.dim_date.sql
@@ -1,13 +1,11 @@
 -- Create table if it does not exist
 IF OBJECT_ID('[stg_bright_mart_sales].[dbo].[dim_date]', 'U') IS NULL
-
     CREATE TABLE [stg_bright_mart_sales].[dbo].[dim_date](
         [DateID] INT IDENTITY(1, 1) PRIMARY KEY,
         [transaction_date] [varchar](50) NULL,
         [customer_since] [varchar](50) NULL,
         [load_date] DATETIME DEFAULT GETDATE()
-    );
-GO
+);
 
 -- Insert distinct values into the table from raw data only if they do not already exist
 INSERT INTO [stg_bright_mart_sales].[dbo].[dim_date] (
@@ -22,7 +20,7 @@ WHERE NOT EXISTS (
     SELECT 1 
     FROM [stg_bright_mart_sales].[dbo].[dim_date] dim
     WHERE ISNULL(dim.[transaction_date], '') = ISNULL(raw.[transaction_date], '')
-      AND ISNULL(dim.[customer_since], '')    = ISNULL(raw.[customer_since], '')
+      AND ISNULL(dim.[customer_since], '')   = ISNULL(raw.[customer_since], '')
 );
 
 -- Show the dim date table
