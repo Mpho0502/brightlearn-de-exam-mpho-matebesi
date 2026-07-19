7d7f285beb3b60e306cfde0c6bb75e7a9de4de63
Mpho <matebesimoe89@gmail.com>
2026-07-19 13:10:02 +0200

    inserted distinct values to dwh bright mart fact and dims fks table

--- 1.0.scripts_sql/1.4.fact_table/dwh_fact/1.4.1.dwh_bright_mart_fact_table.sql
+++ 1.0.scripts_sql/1.4.fact_table/dwh_fact/1.4.1.dwh_bright_mart_fact_table.sql
@@ -21,3 +21,46 @@ IF OBJECT_ID('[dwh_bright_mart_sales].[dbo].[dwh_bright_mart_fact_table]', 'U') 
         [load_date]            DATETIME DEFAULT GETDATE()
 );
 
+-- Insert distinct values into the table from clean data only if they do not already exist
+INSERT INTO [dwh_bright_mart_sales].[dbo].[dwh_bright_mart_fact_table] (
+        [StoreID], 
+        [CustomerID], 
+        [ProductID],
+        [PaymentID], 
+        [DateID], 
+        [unit_price],
+        [cost_price],
+        [qty],
+        [line_amount],
+        [stock_on_hand],
+        [reorder_threshold],
+        [transaction_amount],
+        [transaction_discount],
+        [is_negative_value]
+)
+SELECT DISTINCT
+        cln.[StoreID], 
+        cln.[CustomerID], 
+        cln.[ProductID],
+        cln.[PaymentID], 
+        cln.[DateID], 
+        cln.[unit_price],
+        cln.[cost_price],
+        cln.[qty],
+        cln.[line_amount],
+        cln.[stock_on_hand],
+        cln.[reorder_threshold],
+        cln.[transaction_amount],
+        cln.[transaction_discount],
+        cln.[is_negative_value]
+FROM [stg_bright_mart_sales].[dbo].[clean_bright_mart_fact_table] cln
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM [dwh_bright_mart_sales].[dbo].[dwh_bright_mart_fact_table] fact
+    WHERE 
+          ISNULL(fact.[StoreID], '')              = ISNULL(cln.[StoreID], '')
+      AND ISNULL(fact.[CustomerID], '')           = ISNULL(cln.[CustomerID], '')
+      AND ISNULL(fact.[ProductID], '')            = ISNULL(cln.[ProductID], '')
+      AND ISNULL(fact.[PaymentID], '')            = ISNULL(cln.[PaymentID], '')
+      AND ISNULL(fact.[DateID], '')               = ISNULL(cln.[DateID], '')
+);
