12bc2d4b1f634a4a099f951c07fc5372c0c17e27
Mpho <matebesimoe89@gmail.com>
2026-07-19 12:29:48 +0200

    inserted distinct values using where not exist to dwh dim date

--- 1.0.scripts_sql/1.3.dim_tables/dwh_dims/1.3.5.dwh_dim_date.sql
+++ 1.0.scripts_sql/1.3.dim_tables/dwh_dims/1.3.5.dwh_dim_date.sql
@@ -7,3 +7,18 @@ IF OBJECT_ID('[dwh_bright_mart_sales].[dbo].[dwh_dim_date]', 'U') IS NULL
         [load_date] DATETIME DEFAULT GETDATE()
 );
 
+-- Insert distinct values into the table from raw data only if they do not already exist
+INSERT INTO [dwh_bright_mart_sales].[dbo].[dwh_dim_date] (
+        [transaction_date],
+        [customer_since]
+)
+SELECT DISTINCT
+        cln.[transaction_date],
+        cln.[customer_since]
+FROM [stg_bright_mart_sales].[dbo].[clean_dim_date] cln
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM [dwh_bright_mart_sales].[dbo].[dwh_dim_date] dim
+    WHERE ISNULL(dim.[transaction_date], '') = ISNULL(cln.[transaction_date], '')
+      AND ISNULL(dim.[customer_since], '')   = ISNULL(cln.[customer_since], '')
+);
