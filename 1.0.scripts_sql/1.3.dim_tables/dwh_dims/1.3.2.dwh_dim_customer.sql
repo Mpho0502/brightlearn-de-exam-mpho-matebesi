e43e82e48c050e5d1f92ef2c49aed1bcf6931460
Mpho <matebesimoe89@gmail.com>
2026-07-19 11:31:03 +0200

    inserted distinct values using where not exist to dwh dim customer

--- 1.0.scripts_sql/1.3.dim_tables/dwh_dims/1.3.2.dwh_dim_customer.sql
+++ 1.0.scripts_sql/1.3.dim_tables/dwh_dims/1.3.2.dwh_dim_customer.sql
@@ -12,3 +12,33 @@ IF OBJECT_ID('[dwh_bright_mart_sales].[dbo].[dwh_dim_customer]', 'U') IS NULL
         [load_date]             DATETIME DEFAULT GETDATE()
     );
 
+-- Insert distinct values into the table from raw data only if they do not already exist
+INSERT INTO [dwh_bright_mart_sales].[dbo].[dwh_dim_customer] (
+        [customer_first_name],
+        [customer_last_name],
+        [customer_email],
+        [customer_phone],
+        [customer_city],
+        [customer_province],
+        [customer_loyalty_tier]
+)
+SELECT DISTINCT
+        cln.[customer_first_name],
+        cln.[customer_last_name],
+        cln.[customer_email],
+        cln.[customer_phone],
+        cln.[customer_city],
+        cln.[customer_province],
+        cln.[customer_loyalty_tier]
+FROM [stg_bright_mart_sales].[dbo].[clean_dim_customer] cln
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM [dwh_bright_mart_sales].[dbo].[dwh_dim_customer] dim
+    WHERE ISNULL(dim.[customer_first_name], '')   = ISNULL(cln.[customer_first_name], '')
+      AND ISNULL(dim.[customer_last_name], '')    = ISNULL(cln.[customer_last_name], '')
+      AND ISNULL(dim.[customer_email], '')        = ISNULL(cln.[customer_email], '')
+      AND ISNULL(dim.[customer_phone], '')        = ISNULL(cln.[customer_phone], '')
+      AND ISNULL(dim.[customer_city], '')         = ISNULL(cln.[customer_city], '')
+      AND ISNULL(dim.[customer_province], '')     = ISNULL(cln.[customer_province], '')
+      AND ISNULL(dim.[customer_loyalty_tier], '') = ISNULL(cln.[customer_loyalty_tier], '')
+);
